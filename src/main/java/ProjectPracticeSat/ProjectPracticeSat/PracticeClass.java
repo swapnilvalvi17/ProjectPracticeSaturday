39329ed1dc9bce31271a08803e27768ea7a65d85
Swapnil Valvi <swapnilvalvi17@gmail.com>
2025-01-25 17:27:48 +0530

    project initiated

---
+++ src/main/java/ProjectPracticeSat/ProjectPracticeSat/PracticeClass.java
@@ -0,0 +1,11 @@
+package ProjectPracticeSat.ProjectPracticeSat;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Project has been initiated");
+
+	}
+
+}
